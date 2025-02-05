096cf601672f241c73ae04e7df829ebc02f5e8e3
Sp1inter <splinter2905@yandex.ru>
2025-02-05 12:23:39 +0300

    [8 уровень] [12 задача] Типы данных. - Первые объекты.

---
+++ src/com/javarush/task/pro/task07/task0712/Solution.java
@@ -0,0 +1,41 @@
+package com.javarush.task.pro.task07.task0712;
+
+/* 
+Первые объекты
+
+Используя оператор new в методе main(), создай:
+
+10 объектов типа String;
+5 объектов типа int[] (массивов значений int);
+2 объекта типа Scanner.
+Требования:
+•	В методе main() должно создаваться 10 объектов типа String.
+•	В методе main() должно создаваться 5 объектов типа int[].
+•	В методе main() должно создаваться 2 объекта типа Scanner.
+*/
+
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        String a1 = new String();
+        String a2 = new String();
+        String a3 = new String();
+        String a4 = new String();
+        String a5 = new String();
+        String a6 = new String();
+        String a7 = new String();
+        String a8 = new String();
+        String a9 = new String();
+        String a10 = new String();
+
+        int[] array1 = new int[1];
+        int[] array2 = new int[2];
+        int[] array3 = new int[3];
+        int[] array4 = new int[4];
+        int[] array5 = new int[5];
+
+        Scanner sc = new Scanner(System.in);
+        Scanner sc2 = new Scanner(System.in);
+    }
+}
